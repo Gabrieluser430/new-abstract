ede1563ace4da2b38ae629c3c676fd094c787cbb
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-05-31 23:33:09 -0300

    navbar on cellphones

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
-        Abstract
+    <div>
+      <nav className="flex justify-between items-center py-7 px-12 bg-black text-gray-200">
+        <div className="flex flex-col ">
+          <a className="mb-6 text-xl">Abstract</a>
+          <a className="text-xl">Help Center</a>  
+        </div>
+        <div className="flex flex-col ">
+          <button className="mb-6 text-xl bg-black border-2 border-white py-2 px-4 rounded-lg">Submit a request</button>
+          <button className="text-xl bg-purple-800 rounded-lg py-2">Sign In</button>
+        </div>
       </nav>
-    
-    </main>
+    </div>
   )
 }
