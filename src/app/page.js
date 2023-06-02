25ced441567e0b0c49a17827d698b1def848d8d0
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-06-01 23:26:54 -0300

    better styles inside the nav

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,21 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <nav className="flex justify-between items-center py-7 px-12 bg-black text-gray-200">
+      <nav className="flex justify-between items-center py-7 px-12 bg-black text-white">
         <div className="flex flex-col ">
           <a className="mb-6 text-xl">Abstract</a>
           <a className="text-xl">Help Center</a>  
         </div>
         <div className="flex flex-col ">
           <button className="mb-6 text-xl bg-black border-2 border-white py-2 px-4 rounded-lg">Submit a request</button>
-          <button className="text-xl bg-purple-800 rounded-lg py-2">Sign In</button>
+          <button className="text-xl bg-purple-700 rounded-lg py-2">Sign In</button>
         </div>
       </nav>
+      <main>
+        <section>
+          <h1>How can we help you?</h1>
+        </section>
+      </main>
     </div>
   )
 }
